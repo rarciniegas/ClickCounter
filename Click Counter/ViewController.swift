06a0ad91a25ab631cfc3605d7c6fe810faa54769
Roberto Arciniegas <rarciniegas@hotmail.com>
2015-09-26 19:50:37 -0400

    Initial Commit

---
+++ Click Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Roberto Arciniegas on 9/26/15.
+//  Copyright © 2015 Roberto Arciniegas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
